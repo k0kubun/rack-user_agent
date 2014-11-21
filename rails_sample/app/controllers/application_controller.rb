57636d377e31e24cd9a80c8af987c402b9374748
Takashi Kokubun <takashikkbn@gmail.com>
2014-11-21 17:51:16 +0900

    Update sample apps

--- rails_sample/app/controllers/application_controller.rb
+++ rails_sample/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   REQUEST_METHODS = %i[
-    device_type os os_version browser browser_version browser_vendor
+    device_type os os_version smartphone_version browser browser_version browser_vendor
     from_pc? from_smartphone? from_mobilephone? from_appliance? from_crawler?
+    from_iphone? from_ipad? from_android? from_android_tablet? from_windows_phone?
   ].freeze
 
   def index
--- sinatra_sample/app.rb
+++ sinatra_sample/app.rb
@@ -8,8 +8,9 @@ end
 
 get "/" do
   methods = %i[
-    device_type os os_version browser browser_version browser_vendor
+    device_type os os_version smartphone_version browser browser_version browser_vendor
     from_pc? from_smartphone? from_mobilephone? from_appliance? from_crawler?
+    from_iphone? from_ipad? from_android? from_android_tablet? from_windows_phone?
   ]
   methods.map { |m| "#{m}: #{request.send(m)}" }.join("<br>")
 end
